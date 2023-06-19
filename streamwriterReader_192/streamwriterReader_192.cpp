962ad082c52366df1608f5d1c3c99b91ef21727d
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-06-19 16:40:57 +0700

    Add project files.

---
+++ streamwriterReader_192/streamwriterReader_192.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk objek array yang akan kita gunakan
+using namespace std;
+
+int main()
+{
+	cout << "Awal program" << endl;
+	try {
+		array<int, 3> data = { 1, 2, 3 };
+		cout << data.at(4) << endl;
+
+	}
+	

548cac50f53a6f1de1a0ddf389b813f764936ee4
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-06-19 16:52:55 +0700

    menulis dan memasukkan niilai

--- iofile.cpp/iofile.cpp.cpp
+++ iofile.cpp/iofile.cpp.cpp
@@ -18,3 +18,6 @@ int main() {
 		cout << "- ";
 		//mendapatkan setiap karakter dalam satu baris
 		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan karakter q
+		if (baris ==  "q") break;
+		//menulis dan memasukkan nilai dari 'baris'ke dalam file
